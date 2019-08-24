240052cf3399819657b07fc52f8e4b263052e628
TranTruong <truongtv1909@gmail.com>
2019-08-24 20:06:03 +0700

    fix

--- controllers/controller.user.js
+++ controllers/controller.user.js
@@ -2,6 +2,48 @@ var db = require('../db');
 var userinDB = db.get('users').value();
 var shortid = require('shortid');
 
+// module.exports = {
+// index: function(req,res){
+//     res.render('user/index',{
+//         user: userinDB
+//     });  
+// },
+// getCreate: function(req,res){
+//     res.render('user/create');
+// },
+// getsearch: function(req,res){
+//     var key = req.query.q;
+//     var newarr = userinDB.filter(function(us){
+//        return us.name.toLocaleLowerCase().includes(key.toLocaleLowerCase());      
+//     });
+//     res.render('user/index',{
+//         user: newarr
+        
+//     })   
+// },
+// getUserInfo: function(req,res){
+//     var idpr =req.params.userId;
+//     if(idpr){
+//         var userInfo = db.get('users').find({id:idpr}).value();
+//         res.render('user/info',{
+//             user: userInfo
+//         });
+//     }else{
+//         res.render('user/4004');
+//     }
+// },
+// potCreateUser: function(req,res){
+//     var namenew = req.body.na;
+//     var newuser ={
+//         id: shortid.generate(),
+//         name: namenew,
+//         about: req.body.about
+//     };
+//     db.get('users').push(newuser).write();
+//     res.redirect('/user');  
+// }
+// };
+// ------------------------------------------------------------------------------------
 module.exports.index = function(req,res){
     res.render('user/index',{
         user: userinDB
