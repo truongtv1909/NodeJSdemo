e782a6fb43df3271848b4e1dba523f3f32b1fe4b
TranTruong <truongtv1909@gmail.com>
2019-08-26 23:31:13 +0700

    continus login

--- controllers/controller.login.js
+++ controllers/controller.login.js
@@ -9,5 +9,17 @@ res.render('login/test');
 module.exports.postLogin = function(req, res){
 var email = req.body.email;
 var password = req.body.password;
+var userif = db.get('users').find({email:email}).value();
+var error = [];
+if(!userif){
+    console.log(email);
+    error.push('Email is not exist');
 
+    res.render('login/test',{
+        err: error
+    });
+    return;
+    console.log(em);
+}
+console.log('us is: ',userif);
 }
