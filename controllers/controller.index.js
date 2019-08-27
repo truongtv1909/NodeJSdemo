d7902a5659c348a7343868ae1d74b2f8c17136ad
TranTruong <truongtv1909@gmail.com>
2019-08-27 09:34:59 +0700

    Login 27-08

--- controllers/controller.index.js
+++ controllers/controller.index.js
@@ -1,15 +1,24 @@
 var db = require('../db');
 module.exports.index = function(req,res,next){
     var labe = ''
-    var add = db.get('users').find({id:req.cookies.cooid}).value();
+    var add = '';
+    db.get('users').find({id:req.cookies.cooid}).value();
+
     if(req.cookies.cooid){
         labe = 'Logout';
+        add = db.get('users').find({id:req.cookies.cooid}).value();
+        res.render('index',{
+            name:add.name.toLocaleUpperCase(),
+            labes:labe
+        });
     }else{
         labe = 'Login';
+        add ='Home';
+        res.render('index',{
+            name:add.toLocaleUpperCase(),
+            labes:labe
+        });
     }
-    res.render('index',{
-        name:add.name.toLocaleUpperCase(),
-        labes:labe
-    });
+ 
     next();
 };
