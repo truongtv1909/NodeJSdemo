489d57af2e0173bdba6d6a0c8a4b68383eac2497
TranTruong <truongtv1909@gmail.com>
2019-08-28 20:12:27 +0700

    pagein finis

--- controllers/controller.pet.js
+++ controllers/controller.pet.js
@@ -3,12 +3,12 @@ var pets = petdb.get('petdb').value();
 var shortid = require('shortid');
 
 module.exports.index = function(req, res){
-    console.log('page is: ',req.query.page)
     var page = parseInt(req.query.page) || 1;
     var perPage = 8;
     var start = (page-1)*perPage;
     var end = (page -1)*perPage + perPage;
     var pas = Math.ceil(pets.length/perPage);
+    // var pazzz = 3;
     petindex = petdb.get('petdb').value().slice(start, end);
     res.render('pet/index',{
         pet:petindex,
@@ -26,11 +26,25 @@ module.exports.postCreate = function(req,res){
 };
 
 module.exports.getSearch = function(req,res){
-    // console.log(req.query.q)
-    var name = req.query.q.toLocaleLowerCase();
-    var findpet = pets.filter(e=>e.name.toLocaleLowerCase().includes(name));
+    var page = parseInt(req.query.page) || 1;
+    var perPage = 8;
+    var start = (page-1)*perPage;
+    var end = (page -1)*perPage + perPage;
+    if(req.query.q){
+        var name = req.query.q;
+    }else{
+        var name = req.query.name;
+    }
+    
+    var findpet = pets.filter(e=>e.name.toLocaleLowerCase().includes(name.toLocaleLowerCase()));
+
+    var pas = Math.ceil(findpet.length/perPage);
+    var pagePet = findpet.slice(start,end);
     res.render('pet/index',{
-        pet:findpet
+        pet:pagePet,
+        numberpage:pas,
+        page: page,
+        name: name
     });
 };
 
